34a43c245ff1dde92e2e6ce0f0020c53441d8102
Eloá <eloabrizeiscostasantana@gmail.com>
2025-09-25 22:17:47 -0300

    versão que todos os produtos vão pro carrinho e tem o botão que vai p formulario ebaa

--- routes/homenagensRoutes.js
+++ routes/homenagensRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const Produto = require('../models/produtoModel');
 const { verificarLogin } = require('../middlewares/authMiddleware');
 
-
 router.use(verificarLogin);
 
 router.get('/', (req, res) => {
